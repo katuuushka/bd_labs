bc3ca97b8a198333cabe462718dedc93a878ce90
katuuushka <146069610+katuuushka@users.noreply.github.com>
2023-11-07 13:15:35 +0400

    Delete laba2.8.sql

--- laba2.8.sql
+++
@@ -1,6 +0,0 @@
-/* выбрать имена объектов с дешевой качественной стоимостью*/
-use cd;
-select * from facilities where monthlymaintenance < 100;
-/* выбрать имена объектов с дорогой качественной стоимостью*/
-use cd;
-select * from facilities where monthlymaintenance >= 100;
