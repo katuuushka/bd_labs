18b229ab0b3cfcce3ce0427fd2ca1c512c446081
katuuushka <146069610+katuuushka@users.noreply.github.com>
2023-11-13 11:02:08 +0400

    laba 5

--- lab 5/5-3.sql
+++ lab 5/5-3.sql
@@ -1,5 +1,11 @@
-/* количество рекомендаций для каждого члена клуба, 
-данных этим членом клуба, искючив членов, которые не давали 
-рекомендаций.*/
+/* количество рекомендаций для каждого члена клуба, данных этим членом клуба, искючив членов, которые не давали 
+рекомендаций. */
 USE cd;
-SELECT recommendedby, COUNT(*) FROM members WHERE recommendedby IS NOT NULL GROUP BY recommendedby;
+SELECT m.memid AS MemberID,
+m.firstname AS firstname,
+m.surname AS lastname,
+COUNT(m2.memid) AS r_count
+FROM members m
+JOIN members m2 ON m.memid = m2.recommendedby
+WHERE m.recommendedby IS NOT NULL
+GROUP BY m.memid, m.firstname, m.surname;
