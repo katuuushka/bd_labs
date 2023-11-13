93ed9814f849c98477fe4d9c01bd6e606596232b
katuuushka <146069610+katuuushka@users.noreply.github.com>
2023-11-13 10:18:27 +0400

    laba 3

--- lab 3/3-7.sql
+++ lab 3/3-7.sql
@@ -1,5 +1,5 @@
 /* Выберите список всех членгов, включая человека, который их рекомендовал (если таковой имеется), без использования объединений.
-Исключите в списке дубликаты, упорядлчите лист по ФИО (== имя + фамилия)*/
+Исключите в списке дубликаты, упорядлчите лист по ФИО (== имя + фамилия) */
 USE cd;
 SELECT CONCAT(mem.firstname,' ',mem.surname) AS membername,
 (SELECT CONCAT(rec.firstname,' ',rec.surname) FROM members rec WHERE mem.recommendedby=rec.memid) AS recname 
