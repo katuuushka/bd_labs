c7a0fd3a665abefd50bb2e381a2de8c2c00d56e6
katuuushka <146069610+katuuushka@users.noreply.github.com>
2023-11-13 10:04:44 +0400

    3-7.sql

--- lab 3/3-7.sql
+++ lab 3/3-7.sql
@@ -1,8 +1,8 @@
- /* Выберите список всех членгов, включая человека, который их рекомендовал (если таковой имеется), без использования объединений.
- Исключите в списке дубликаты, упорядлчите лист по ФИО (== имя + фамилия)*/
- USE cd;
- SELECT concat(m1.surname, '', m1.firstname) AS FIOmember
-FROM members m1 WHERE 
-EXISTS( SELECT 1 FROM members AS m2 WHERE m2.memid = m1.recommendedby)
-OR NOT EXISTS(SELECT 1 FROM members AS m3 WHERE m3.memid = m1.recommendedby)
-ORDER BY FIOmember;
+/* Выберите список всех членгов, включая человека, который их рекомендовал (если таковой имеется), без использования объединений.
+Исключите в списке дубликаты, упорядлчите лист по ФИО (== имя + фамилия)*/
+USE cd;
+SELECT CONCAT(mem.firstname,' ',mem.surname) AS membername,
+(SELECT CONCAT(rec.firstname,' ',rec.surname) FROM members rec WHERE mem.recommendedby=rec.memid) AS recname 
+FROM members mem
+WHERE mem.memid!=0
+ORDER BY membername;
