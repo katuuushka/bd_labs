5ef26876109fd5416ae96906666f2bf9f5d3f9a4
katuuushka <146069610+katuuushka@users.noreply.github.com>
2023-11-21 11:50:38 +0400

    laba 5

--- lab 5/5-7.sql
+++ lab 5/5-7.sql
@@ -1,4 +1,4 @@
 /* общее количество участников (члены + гости), 
 совершивших хотя бы одно бронирование */
 USE cd;
-SELECT COUNT(memid) FROM bookings;
+SELECT COUNT(DISTINCT memid) AS 'Количество участников, совершивших хотя бы одно бронирование' FROM bookings;
