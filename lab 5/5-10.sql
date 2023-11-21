ca3a901abc9e787a01b684a09e1e7d23339907bc
katuuushka <146069610+katuuushka@users.noreply.github.com>
2023-11-21 11:51:29 +0400

    laba 5

--- lab 5/5-10.sql
+++ lab 5/5-10.sql
@@ -1,12 +1,18 @@
 /*  список общего количества мест, забронированных на 
 один объект в месяц в 2012 году*/
 USE cd;
-SELECT bookings.facid,
-MONTH(bookings.starttime) AS month,
-COUNT(*) AS slots
-FROM bookings
-INNER JOIN facilities ON bookings.facid = facilities.facid
-WHERE YEAR(bookings.starttime) = 2012
-GROUP BY bookings.facid, MONTH(bookings.starttime)
-WITH ROLLUP
-ORDER BY bookings.facid, MONTH(bookings.starttime);
+SELECT 	IFNULL(derived_table.facid, 'Total') as facid,
+		IFNULL(derived_table.month, 'Total') as month,
+        SUM(derived_table.slots) AS 'Количество забронированных мест'
+FROM 
+(SELECT 
+        f.facid AS facid,
+        MONTH(b.starttime) AS month,
+        SUM(b.slots) as slots
+    FROM
+        facilities f
+    INNER JOIN bookings b ON f.facid = b.facid
+    WHERE
+        YEAR(b.starttime) = 2012
+    GROUP BY facid, month, slots) AS derived_table
+GROUP BY facid, month WITH ROLLUP;
