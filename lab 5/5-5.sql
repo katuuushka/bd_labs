6080d7a02cbe42ab97e8fa065cca458c93d2457f
katuuushka <146069610+katuuushka@users.noreply.github.com>
2023-11-21 11:49:16 +0400

    laba 5

--- lab 5/5-5.sql
+++ lab 5/5-5.sql
@@ -1,5 +1,5 @@
 /* количество аренд каждого из объектов клуба за 
 сентябрь 2012 года */
 USE cd;
-SELECT facid, COUNT(*) FROM bookings GROUP BY facid; 
-WHERE joindate BETWEEN '2012-09-01' AND '2012-09-30'GROUP BY facid;
+SELECT facility, sum(b.slots) AS 'Количество аренд' FROM facilities f 
+INNER JOIN bookings AS b ON b.facid = f.facid WHERE DATE(b.starttime) >= '2012-09-01' AND DATE(b.starttime) <= '2012-09-30' GROUP BY f.facid;
