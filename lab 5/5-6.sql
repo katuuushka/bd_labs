50b08aa3cb567a9d3f6aee032051760138c69bbe
katuuushka <146069610+katuuushka@users.noreply.github.com>
2023-11-21 11:49:58 +0400

    laba 5

--- lab 5/5-6.sql
+++ lab 5/5-6.sql
@@ -1,6 +1,6 @@
 /* список общего количества бронирований на каждый 
 объект в месяц в 2012 году */
 USE cd;
-SELECT EXTRACT(MONTH FROM starttime) AS MONTH, bookings.facid, COUNT(*) AS TOTAL_BOOKINGS FROM bookings
-WHERE EXTRACT(YEAR FROM starttime) = 2012
-GROUP BY MONTH, facid;
+SELECT f.facility, MONTH(b.starttime) AS 'Месяц', SUM(b.slots) AS 'Количество бронирований' FROM facilities f
+INNER JOIN bookings b ON f.facid = b.facid WHERE YEAR(b.starttime) = 2012
+GROUP BY f.facility, MONTH(b.starttime);
