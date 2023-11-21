2a4c9698dc942a44bc3dbba8899f89c46b1fa969
katuuushka <146069610+katuuushka@users.noreply.github.com>
2023-11-21 11:48:27 +0400

    laba 5

--- lab 5/5-4.sql
+++ lab 5/5-4.sql
@@ -1,6 +1,4 @@
 /* количество аренд каждого из объектов клуба */
 USE cd;
-SELECT f.facid AS Facid,
-f.facility AS Facility, COUNT(b.slots) AS Rec
-FROM facilities f
-JOIN bookings b ON b.facid = f.facid GROUP BY f.facid, f.facility;
+SELECT facility, sum(b.slots) AS 'Количество аренд' FROM facilities f 
+INNER JOIN bookings AS b ON b.facid = f.facid GROUP BY f.facid;
