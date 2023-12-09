217e3fcd7cd22b683b1bc55bb86fa0a46bc6cfaa
katuuushka <146069610+katuuushka@users.noreply.github.com>
2023-12-09 16:50:18 +0400

    5-10.sql

--- lab 5/5-10.sql
+++ lab 5/5-10.sql
@@ -1,18 +1,23 @@
-/*  список общего количества мест, забронированных на 
-один объект в месяц в 2012 году*/
+-- список общего количества мест, забронированных на один объект в месяц в 2012 году.
 USE cd;
-SELECT 	IFNULL(derived_table.facid, 'Total') as facid,
-		IFNULL(derived_table.month, 'Total') as month,
-        SUM(derived_table.slots) AS 'Количество забронированных мест'
+-- Основной запрос:
+SELECT 	 
+	IFNULL(derived_table.facid, 'Total') as facid, -- Используется IFNULL для предотвращения NULL идентификаторов объектов.
+	IFNULL(derived_table.month, 'Total') as month, -- IFNULL обеспечивает замену NULL значений месяцев на 'Total'.
+	SUM(derived_table.slots) AS 'Количество забронированных мест' -- Суммируем количество забронированных слотов.
 FROM 
-(SELECT 
-        f.facid AS facid,
-        MONTH(b.starttime) AS month,
-        SUM(b.slots) as slots
-    FROM
-        facilities f
-    INNER JOIN bookings b ON f.facid = b.facid
-    WHERE
-        YEAR(b.starttime) = 2012
-    GROUP BY facid, month, slots) AS derived_table
-GROUP BY facid, month WITH ROLLUP;
+(
+        -- Подзапрос (derived_table): 
+        SELECT 
+            f.facid AS facid,
+            MONTH(b.starttime) AS month,
+            SUM(b.slots) as slots
+        FROM
+            facilities f
+        INNER JOIN bookings b ON f.facid = b.facid
+        WHERE
+            YEAR(b.starttime) = 2012
+        GROUP BY facid, month, slots
+    ) AS derived_table -- Результат подзапроса алиасируется как 'derived_table'.
+
+GROUP BY facid, month WITH ROLLUP; -- Группировка результатов по идентификатору объекта и месяцу с использованием WITH ROLLUP.
