b9ff85618ac29bd9457218225038fd6e7580745d
katuuushka <146069610+katuuushka@users.noreply.github.com>
2023-12-09 16:53:52 +0400

    laba 5

--- lab 5/5-10.sql
+++ lab 5/5-10.sql
@@ -1,13 +1,13 @@
--- список общего количества мест, забронированных на один объект в месяц в 2012 году.
+/* список общего количества мест, забронированных на один объект в месяц в 2012 году.*/
 USE cd;
--- Основной запрос:
+/*Основной запрос:*/
 SELECT 	 
-	IFNULL(derived_table.facid, 'Total') as facid, -- Используется IFNULL для предотвращения NULL идентификаторов объектов.
-	IFNULL(derived_table.month, 'Total') as month, -- IFNULL обеспечивает замену NULL значений месяцев на 'Total'.
-	SUM(derived_table.slots) AS 'Количество забронированных мест' -- Суммируем количество забронированных слотов.
+	IFNULL(derived_table.facid, 'Total') as facid, /* Используется IFNULL для предотвращения NULL идентификаторов объектов.*/
+	IFNULL(derived_table.month, 'Total') as month, /* IFNULL обеспечивает замену NULL значений месяцев на 'Total'.*/
+	SUM(derived_table.slots) AS 'Количество забронированных мест' /* Суммируем количество забронированных слотов.*/
 FROM 
 (
-        -- Подзапрос (derived_table): 
+        /* Подзапрос (derived_table): */
         SELECT 
             f.facid AS facid,
             MONTH(b.starttime) AS month,
@@ -18,6 +18,6 @@ FROM
         WHERE
             YEAR(b.starttime) = 2012
         GROUP BY facid, month, slots
-    ) AS derived_table -- Результат подзапроса алиасируется как 'derived_table'.
+    ) AS derived_table /*Результат подзапроса алиасируется как 'derived_table'.*/
 
-GROUP BY facid, month WITH ROLLUP; -- Группировка результатов по идентификатору объекта и месяцу с использованием WITH ROLLUP.
+GROUP BY facid, month WITH ROLLUP; /* Группировка результатов по идентификатору объекта и месяцу с использованием WITH ROLLUP.*/
