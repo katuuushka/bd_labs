e27acf136823f685bd6ee9c31b1e55eed25f3f66
katuuushka <146069610+katuuushka@users.noreply.github.com>
2023-12-11 20:32:43 +0400

    laba 6

--- laba 6/6-2.sql
+++ laba 6/6-2.sql
@@ -1,6 +1,9 @@
 /*Выберите количество аренд объектов клуба в каждый месяц.*/
 USE cd;
-SELECT YEAR(b.starttime) AS 'Год', MONTH(b.starttime) AS 'Месяц', f.facility AS 'Название объекта', COUNT(*) AS 'Количество аренд в каждый месяц'
-FROM bookings b
-INNER JOIN facilities f ON b.facid = f.facid
-GROUP BY YEAR(starttime), MONTH(starttime), facility;
+SELECT
+    facility
+,   SUM(book.slots) as rent_count
+,   DATE_FORMAT(book.starttime, '%y %m') as month
+FROM cd.facilities as fac
+JOIN cd.bookings as book ON book.facid = fac.facid
+GROUP BY fac.facid, month;
