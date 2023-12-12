83535833182c44b75abd9221aba0383cf50fcc96
katuuushka <146069610+katuuushka@users.noreply.github.com>
2023-12-12 18:09:20 +0400

    Delete laba 6/6-2.sql

--- laba 6/6-2.sql
+++
@@ -1,9 +0,0 @@
-/*Выберите количество аренд объектов клуба в каждый месяц.*/
-USE cd;
-SELECT
-    facility
-,   SUM(book.slots) as rent_count
-,   DATE_FORMAT(book.starttime, '%y %m') as month
-FROM cd.facilities as fac
-JOIN cd.bookings as book ON book.facid = fac.facid
-GROUP BY fac.facid, month;
