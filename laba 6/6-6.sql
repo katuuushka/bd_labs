fbe2a1e7a573251c76c685037f314160d77321b1
katuuushka <146069610+katuuushka@users.noreply.github.com>
2023-12-12 18:10:11 +0400

    Delete laba 6/6-6.sql

--- laba 6/6-6.sql
+++
@@ -1,6 +0,0 @@
-/*Выведите все объектов клуба, если они содержат 
-в названии слово  ‘Tennis’ в произвольном регистре.*/
-USE cd;
-SELECT facility AS 'Объекты' 
-FROM facilities
-WHERE facility LIKE '%Tennis%';
