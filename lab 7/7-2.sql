8ea058bf151cd90de862ee47c15e7e929da231c6
katuuushka <146069610+katuuushka@users.noreply.github.com>
2023-12-21 17:55:49 +0400

    laba 7

--- lab 7/7-2.sql
+++ lab 7/7-2.sql
@@ -1,54 +1,60 @@
-/*Создание таблицы payments.*/
+/*Создание таблицы paymentsи добавление payed*/
 USE cd;
-
-DROP TABLE IF EXISTS payments;
-CREATE TABLE IF NOT EXISTS payments (
-    payid INT PRIMARY KEY AUTO_INCREMENT,
-    bookid INT,
-    payment DECIMAL DEFAULT 0,
-    FOREIGN KEY (bookid) REFERENCES bookings (bookid)
+CREATE TABLE payments (
+  payid INT PRIMARY KEY AUTO_INCREMENT,
+  bookid INT,
+  payment DECIMAL,
+  FOREIGN KEY (bookid) REFERENCES cd.bookings(bookid)
 );
-/*Редактирование таблицы bookings если она еще не имеет этих изменений.*/
-ALTER TABLE bookings ADD payed TINYINT DEFAULT 0;
+ALTER TABLE bookings ADD COLUMN payed TINYINT DEFAULT 0;
+/* запрещаем удаление оплащенных*/
 DELIMITER $$
-/*Событие на запрет удаления оплаченного бронирования.*/
-DROP TRIGGER IF EXISTS onBookingDelete $$
-CREATE TRIGGER onBookingDelete BEFORE DELETE ON bookings FOR EACH ROW
+DROP TRIGGER IF EXISTS prevent_payment_deletion $$
+CREATE TRIGGER prevent_payment_deletion 
+BEFORE DELETE ON bookings 
+FOR EACH ROW 
 BEGIN
-    IF (OLD.payed = 1) THEN
-        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = "You can't delete already paid booking";
-    END IF;
-END $$
-/* Событие при изменении статуса оплаты в bookings.*/
-DROP TRIGGER IF EXISTS onBookingPay $$
-CREATE TRIGGER onBookingPay AFTER UPDATE ON bookings FOR EACH ROW
+  IF (OLD.payed = 1) THEN
+    SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Can not delete a paid booking!';
+  END IF;
+END$$
+DELIMITER ;
+DELIMITER $$
+/*удаление и замена*/
+DROP TRIGGER IF EXISTS payment_record $$
+CREATE TRIGGER payment_record 
+AFTER UPDATE ON bookings
+FOR EACH ROW
 BEGIN
-    /* Если бронь изменила свой статус оплаты, то удаляет\добавляет оплату (в ином случае делает ничего).*/
     CASE
         WHEN NEW.payed = OLD.payed THEN BEGIN END;
         WHEN NEW.payed = 1 THEN
             INSERT INTO payments(bookid, payment)
-            VALUES(NEW.bookid, getRentCost(NEW.memid, NEW.facid, NEW.slots));
+            VALUES(NEW.bookid, CalculateRentalCost(NEW.memid, NEW.facid, NEW.slots));
         WHEN NEW.payed = 0 THEN
-            DELETE FROM payments as pay WHERE pay.bookid = NEW.bookid;
+            DELETE FROM payments p WHERE p.bookid = NEW.bookid;
     END CASE;
 END $$
-DROP TRIGGER IF EXISTS onBookingCreatePayed $$
-CREATE TRIGGER onBookingCreatePayed AFTER INSERT ON bookings FOR EACH ROW
+/*заполняем payments*/
+DROP TRIGGER IF EXISTS payed_in_book $$
+CREATE TRIGGER payed_in_book 
+AFTER INSERT ON bookings 
+FOR EACH ROW
 BEGIN
-    /*Если бронь уже оплачена при регистрации (payed = 1), добавляет оплату.*/
     IF NEW.payed = 1 THEN
         INSERT INTO payments(bookid, payment)
-        VALUES(NEW.bookid, getRentCost(NEW.memid, NEW.facid, NEW.slots));
+        VALUES(NEW.bookid, CalculateRentalCost(NEW.memid, NEW.facid, NEW.slots));
     END IF;
 END $$
 DELIMITER ;
-/* Изменение статуса записей*/
-UPDATE bookings
-  SET payed = 1
-  WHERE DATE(starttime) < '2012-08-01' AND DATE(starttime) >= '2012-07-01';
-/* Подсчет суммы оплаченных аренд двумя способами*/
-SELECT SUM(payment) FROM payments;
-SELECT SUM(getRentCost(memid, facid, slots))
-    FROM bookings
-    WHERE DATE(starttime) < '2012-08-01' AND DATE(starttime) >= '2012-07-01';
+/*отмечаем, что все аренды июля 2012 оплачены*/
+UPDATE bookings 
+SET payed = 1
+WHERE YEAR(starttime) = 2012 AND MONTH(starttime) = 7;
+/*оплата на июль 2012 с данными из payments*/
+SELECT SUM(payment) as costJuly1
+FROM payments;
+/*оплата на июль 2012 с данными из bookings*/
+SELECT SUM(CalculateRentalCost(memid, facid, slots)) as costJuly2
+FROM bookings 
+WHERE YEAR(starttime) = 2012 AND MONTH(starttime) = 7;
